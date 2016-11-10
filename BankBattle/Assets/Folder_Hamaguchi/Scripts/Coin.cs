11ed5285d224cd19fc65fd8ae393a891d7cd0830
HamaRyunosuke <ryn459@gmail.com>
2016-11-10 11:17:04 +0900

    各コインにプレイヤーが取得したことを判別するためのtriggerを取り付けプレハブに反映。 貯金箱のモデル追加、それに使用するmaterialなどの設定の追加。　 ブロックとコインにLayerを設定　濵口

--- BankBattle/Assets/Folder_Hamaguchi/Scripts/Coin.cs
+++ BankBattle/Assets/Folder_Hamaguchi/Scripts/Coin.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class Coin : MonoBehaviour {
 
+
     //STATE = TAKENでの動きを調整するための変数。
+#if false
     [SerializeField]
     AnimationCurve upCurve; // 上昇するときの動き方。
     [SerializeField]
     float upSpeed; // 上昇するときの速度
     [SerializeField]
     float upAmount; //上昇する高さ
+#endif
 
     //STATE = APPROACHでの動きを調整するための変数。
     [SerializeField]
@@ -21,15 +24,16 @@ public class Coin : MonoBehaviour {
     public int rotSpeed;
     public enum coinState {
         NONE = 0,
-        TAKEN,
         APPROACH,
+
+        TAKEN,
     }
     public coinState state = coinState.NONE;
 
-    GameObject takePlayer;
-    float time;
+    GameObject takePlayer; // コインを取得したプレイヤーを入れる変数。
+    float time; //コインが移動するときに使うTime.deltatimeを入れる変数。
 
-    Vector3 myPos;
+    Vector3 myPos; //自分の座標
     Vector3 endPos;
 
     Vector3 testPos;
@@ -38,6 +42,7 @@ public class Coin : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        takePlayer = GameObject.Find("Capsule");
 	}
 	
 	// Update is called once per frame
@@ -47,46 +52,69 @@ public class Coin : MonoBehaviour {
         switch (state)
         {
             case coinState.NONE:
+#if false
+
                 //ステージに置かれてる状態のときに自分のpositionをとっておく。
                 myPos = this.transform.position;
                 endPos = new Vector3(this.transform.position.x, this.transform.position.y + upAmount, this.transform.position.z);
                 testPos = Vector3.Lerp(myPos, endPos, 0.5f);
+#endif
+                break;
+            case coinState.APPROACH:
+                //上昇しきったあと、プレイヤーに吸い寄せられていくとき。
+                //this.GetComponent<MeshCollider>().isTrigger = true;
+                Vector3 approachPoint = new Vector3(takePlayer.transform.position.x, takePlayer.transform.position.y + 0.7f, takePlayer.transform.position.z);
+                time += Time.deltaTime * approSpeed;
+                float approCurvePos = approCurve.Evaluate(time);
+                this.transform.position = Vector3.Lerp(this.transform.position, approachPoint, approCurvePos);
+                break;
+#if false
 
+            case coinState.TAKEN:
                 //回転させる処理。rotSpeedはエンジン上で変更可能。
                 transform.Rotate(new Vector3(0, rotSpeed, 0));
-                break;
-            case coinState.TAKEN:
+
                 time += Time.deltaTime * upSpeed;
                 float curvePos = upCurve.Evaluate(time);
                 this.transform.position = Vector3.Lerp(myPos, testPos, curvePos);
 
+                //自分の今のポジションがある一定の距離まで上昇したら
                 //コインが上がりきったら次のSTATEに行くようにする。
                 if(this.transform.position.y == testPos.y)
                 {
                     this.GetComponent<Rigidbody>().useGravity = false;
-                    state = coinState.APPROACH;
                     time = 0;
+                    state = coinState.APPROACH;
                 }
                 //この上昇する動きが終わったら次にどういう動きをする？？
                 break;
-            case coinState.APPROACH:
-                //上昇しきったあと、プレイヤーに吸い寄せられていくとき。
-                this.GetComponent<MeshCollider>().isTrigger = true;
-                time += Time.deltaTime * approSpeed;
-                float approCurvePos = approCurve.Evaluate(time);
-                this.transform.position = Vector3.Lerp(this.transform.position, takePlayer.transform.position/*ここには取得したプレイヤーのポジションが入る*/, approCurvePos);
-                break;
+#endif
             default:
                 break;
         }
 	}
 
-    void OnCollisionEnter(Collision col)
+    //コインをプレイヤーが取得した時の処理。
+    void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == "Player")
+        if (col.gameObject.tag == "Player" && state == coinState.NONE)
         {
-            state = coinState.TAKEN;
+            //stateを変える。
+            state = coinState.APPROACH;
+            //コインをとったプレイヤーが誰かを取得。
             takePlayer = col.gameObject;
+            //取得者が重複するのを防ぐため、Triggerを削除する。
+            GameObject takeTrigger = transform.FindChild("TakeTrigger").gameObject;
+            GameObject.Destroy(takeTrigger);
         }
     }
+
+    //void OnCollisionEnter(Collision col)
+    //{
+    //    if(col.gameObject.tag == "Player")
+    //    {
+    //        state = coinState.APPROACH;
+    //        takePlayer = col.gameObject;
+    //    }
+    //}
 }
---
+++ BankBattle/Assets/Folder_Hamaguchi/Scripts/TestMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMove : MonoBehaviour {
+    Vector3 myPos;
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        myPos = this.transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            myPos.x += moveSpeed;
+            this.transform.position = myPos;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            myPos.x -= moveSpeed;
+            this.transform.position = myPos;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            myPos.z += moveSpeed;
+            this.transform.position = myPos;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            myPos.z -= moveSpeed;
+            this.transform.position = myPos;
+        }
+
+    }
+}
