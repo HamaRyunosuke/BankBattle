01f2c08ea39c4609609e6e3ed984db39251e8674
HamaRyunosuke <ryn459@gmail.com>
2016-11-14 13:10:37 +0900

    CoinManagerの更新（ステージに一定数のコインがばら撒かれるところを実装中です。） CoinManagerで新しく使用する6種類のコインのプレハブ追加　　 濱口

--- BankBattle/Assets/Folder_Hamaguchi/Scripts/CoinManager.cs
+++ BankBattle/Assets/Folder_Hamaguchi/Scripts/CoinManager.cs
@@ -2,22 +2,36 @@
 using System.Collections;
 
 public class CoinManager : MonoBehaviour {
+    [SerializeField]
+    int totalCoinAmount; // 作り出すコインすべてをあわせた量(枚数ではなく価値)
+    [SerializeField]
+    int[] coinMax; // 各コインの枚数上限を決める。
+    public GameObject[] createCoins; // 作り出すコイン一覧。6種類。
 
     public GameObject[] Coins;
     int count;
 
+
+
 	// Use this for initialization
 	void Start () {
+        CreateCoin();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(count <= 150)
+        //if (count <= 150)
+        //{
+        //    Instantiate(Coins[0], transform.position, Quaternion.identity);
+        //    count++;
+        //}
+        if(totalCoinAmount >= 0)
         {
-            Instantiate(Coins[0], transform.position, Quaternion.identity);
-            count++;
+            Instantiate(createCoins[1], transform.position, Quaternion.identity);
+            totalCoinAmount -= 75;
         }
+
 
         //int rand = Random.Range(0, 100);
         //if(rand <= 80)
@@ -31,5 +45,22 @@ public class CoinManager : MonoBehaviour {
         //    Instantiate(Coins[2], transform.position, Quaternion.identity);
         //}
 
+    }
+
+    void CreateCoin()
+    {
+        //金の大きいコインを生成。
+        for (int i = 0; i < coinMax[0]; ++i)
+        {
+            Instantiate(createCoins[0], transform.position, Quaternion.identity);
+            totalCoinAmount -= 300;
+        }
+        Debug.Log(totalCoinAmount);
+        //銀大は７５の価値がある。一旦実験として金大を作って差し引かれたtotalCoinAmountの分をすべて銀大にしてみる。
+        //for (int i = totalCoinAmount; 0 <= i; totalCoinAmount -= 75)
+        //{
+        //    Instantiate(createCoins[1], transform.position, Quaternion.identity);
+        //   // totalCoinAmount -= 75;
+        //}
     }
 }
