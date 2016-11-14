d091d7c4a86292a23b05e42dcfc1e34de890c243
HamaRyunosuke <ryn459@gmail.com>
2016-11-14 17:53:07 +0900

    コインが出る数を調整（実装中）　濵口

--- BankBattle/Assets/Folder_Hamaguchi/Scripts/CoinManager.cs
+++ BankBattle/Assets/Folder_Hamaguchi/Scripts/CoinManager.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class CoinManager : MonoBehaviour {
     [SerializeField]
     int totalCoinAmount; // 作り出すコインすべてをあわせた量(枚数ではなく価値)
+
+    //各コインの情報
     [SerializeField]
     int[] coinMax; // 各コインの枚数上限を決める。
+    [SerializeField]
+    int[] coinValue; // コインの価値を付加する。
     public GameObject[] createCoins; // 作り出すコイン一覧。6種類。
 
     public GameObject[] Coins;
@@ -26,11 +30,6 @@ public class CoinManager : MonoBehaviour {
         //    Instantiate(Coins[0], transform.position, Quaternion.identity);
         //    count++;
         //}
-        if(totalCoinAmount >= 0)
-        {
-            Instantiate(createCoins[1], transform.position, Quaternion.identity);
-            totalCoinAmount -= 75;
-        }
 
 
         //int rand = Random.Range(0, 100);
@@ -47,20 +46,44 @@ public class CoinManager : MonoBehaviour {
 
     }
 
+    void CoinGenerate(int coinNo)
+    {
+        int createCount = 0;
+        while (totalCoinAmount > 0 && createCount < coinMax[coinNo])
+        {
+            Instantiate(createCoins[coinNo], transform.position, Quaternion.identity);
+            totalCoinAmount -= coinValue[coinNo];
+            createCount++;
+        }
+        createCount = 0;
+    }
+
     void CreateCoin()
     {
+        int createCount = 0;
         //金の大きいコインを生成。
-        for (int i = 0; i < coinMax[0]; ++i)
+        while (totalCoinAmount > 0 && createCount < coinMax[0])
         {
             Instantiate(createCoins[0], transform.position, Quaternion.identity);
-            totalCoinAmount -= 300;
+            totalCoinAmount -= coinValue[0];
+            createCount++;
         }
-        Debug.Log(totalCoinAmount);
+        createCount = 0;
+        CoinGenerate(0);
         //銀大は７５の価値がある。一旦実験として金大を作って差し引かれたtotalCoinAmountの分をすべて銀大にしてみる。
-        //for (int i = totalCoinAmount; 0 <= i; totalCoinAmount -= 75)
-        //{
-        //    Instantiate(createCoins[1], transform.position, Quaternion.identity);
-        //   // totalCoinAmount -= 75;
-        //}
+        while(totalCoinAmount > 0 && createCount < coinMax[1])
+        {
+            Instantiate(createCoins[1], transform.position, Quaternion.identity);
+            totalCoinAmount -= coinValue[1];
+            createCount++;
+        }
+        createCount = 0;
+        while (totalCoinAmount > 0 && createCount < coinMax[2])
+        {
+            Instantiate(createCoins[2], transform.position, Quaternion.identity);
+            totalCoinAmount -= coinValue[2];
+            createCount++;
+        }
+        createCount = 0;
     }
 }
